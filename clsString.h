2ca958119f0b9276a1d0056947181debe853d04c
Clean Stack <cleanstack.webdev@gmail.com>
2026-06-12 12:06:55 +0100

    Update Split Method

--- clsString.h
+++ clsString.h
@@ -351,34 +351,34 @@ public:
         return CountWords(_Value);
     };
 
-    static vector<string> Split(string S1, string delim)
+    static vector<string> Split(string S1, string Delim)
     {
-        vector<string> vString;
 
-        short pos = 0;
-        string sWord; // define a string variable  
+    vector<string> vString;
 
-        // use find() function to get the position of the delimiters  
-        while ((pos = S1.find(delim)) != std::string::npos)
-        {
-            sWord = S1.substr(0, pos); // store the word 
+    short pos = 0;
+    string sWord; // define a string variable  
 
-            if (sWord != "")
-            {
-                vString.push_back(sWord);
-            }
+    // use find() function to get the position of the delimiters  
+    while ((pos = S1.find(Delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos); // store the word   
+        // if (sWord != "")
+        // {
+        vString.push_back(sWord);
+        //}
 
-            S1.erase(0, pos + delim.length());  // erase() until positon and move to next word
-        }
-
-        if (S1 != "")
-        {
-            vString.push_back(S1); // it adds last word of the string 
-        }
-
-        return vString;
+        S1.erase(0, pos + Delim.length());  /* erase() until positon and move to next word. */
     }
 
+    if (S1 != "")
+    {
+        vString.push_back(S1); // it adds last word of the string.
+    }
+
+    return vString;
+
+    }
     vector<string> Split(string delim)
     {
         return Split(_Value, delim);
